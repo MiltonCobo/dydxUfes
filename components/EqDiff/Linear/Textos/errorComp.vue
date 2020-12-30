bf8b01809dea6fef2c228cbb3a76dea72da29376
MiltonCobo <milton.e.cobo@gmail.com>
2020-12-30 17:31:37 -0300

    new components errorComp e loadingComp in Linear/Textos/

---
+++ components/EqDiff/Linear/Textos/errorComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row></v-row>
+        <v-col align="center">
+          <h3 style="color:green;" align="center">
+            Aconteceu um erro ao carregar a figura.
+          </h3>
+          <v-divider style="margin-bottom: 0.5cm;" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
---
+++ components/EqDiff/Linear/Textos/loadingComp.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row align="center">
+        <p style="color: red;">...Carregando....</p>
+        <v-img src="/figures/Slinear1.png" contain width="400" height="400" />
+      </v-row>
+    </v-container>
+  </div>
+</template>
